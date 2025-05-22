f5f36a723a197afbeb796982b6c50810dd7d3e23
Achiv Gupta <achivgupta@Achivs-MacBook-Air.local>
2025-05-22 11:04:42 +0530

    initial

---
+++ hello_world/app/page.tsx
@@ -0,0 +1,8 @@
+// File: app/page.tsx (React Component in Next.js)
+export default function HomePage() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Hello World Project using react js</h1>
+    </main>
+  );
+}
